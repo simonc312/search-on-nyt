fc2e87388ad009eba60b0a661c68864a37b9a3e1
Simon Chen <simon.c312@berkeley.edu>
2016-02-18 21:52:15 -0800

    refactor MainActivity

--- app/src/main/java/com/simonc312/searchnyt/activities/MainActivity.java
+++ app/src/main/java/com/simonc312/searchnyt/activities/MainActivity.java
@@ -1,71 +1,30 @@
 package com.simonc312.searchnyt.activities;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.simonc312.searchnyt.R;
 import com.simonc312.searchnyt.fragments.TrendingFragment;
 
-import butterknife.Bind;
 import butterknife.BindString;
-import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity
-        implements TrendingFragment.InteractionListener{
-    @BindString(R.string.action_back_pressed)String ACTION_BACK_PRESSED;
+public class MainActivity extends BaseActivity {
     @BindString(R.string.app_name_title) String APP_NAME_TITLE;
-    @Bind(R.id.toolbar)
-    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
         setupSupportActionBar();
-        setTitle();
+        setTitle(APP_NAME_TITLE);
         swapFragment(TrendingFragment.newInstance(TrendingFragment.STAGGERED_LAYOUT, null, TrendingFragment.TRENDING_TYPE));
-    }
-
-    private void setupSupportActionBar(){
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setShowHideAnimationEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-    }
-
-    private void setTitle(){
-        TextView tx = (TextView)findViewById(R.id.app_name);
-        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/EnglishTowne.ttf");
-        tx.setTypeface(custom_font);
-        tx.setText(APP_NAME_TITLE);
-    }
-
-    @Override
-    public void onBackPressed(){
-        if(getSupportFragmentManager().getBackStackEntryCount() > 0){
-            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(ACTION_BACK_PRESSED));
-        }
-        else {
-            super.onBackPressed();
-        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                onBackPressed();
-                return true;
             case R.id.action_search:
                 startActivity(new Intent(this, SearchQueryActivity.class));
                 return true;
@@ -83,20 +42,5 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onLayoutChange(boolean show) {
         getSupportActionBar().setDisplayHomeAsUpEnabled(show);
-    }
-
-    @Override
-    public void onBackPress(boolean pop){
-        if(pop){
-            finish();
-        }
-    }
-
-    private void swapFragment(Fragment fragment){
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container, fragment)
-                .addToBackStack(null)
-                .commit();
     }
 }
